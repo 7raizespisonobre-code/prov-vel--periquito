1ef11b540f25ee2b8b25d49066ce278bebb83741
7raizespisonobre-code <7raizespisonobre@gmail.com>
2026-09-07 16:53:30 -0300

    Add Home component with main content

---
+++ app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>7 Raízes Piso Nobre</h1>
+      <p>
+        Pisos de madeira, restauração, decks, escadas e acabamentos.
+      </p>
+    </main>
+  );
+}
